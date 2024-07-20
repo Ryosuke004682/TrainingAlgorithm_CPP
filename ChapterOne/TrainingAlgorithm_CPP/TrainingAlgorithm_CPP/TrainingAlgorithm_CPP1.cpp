bfbd28be6875cab51bbfde6ffdd8021b5885d007
DESKTOP-PPMD04S\Kr004 <Kr004682zero@icloud.com>
2024-07-20 14:28:43 +0900

    chapter1の初回コミット

---
+++ ChapterOne/TrainingAlgorithm_CPP/TrainingAlgorithm_CPP/TrainingAlgorithm_CPP1.cpp
@@ -0,0 +1,14 @@
+﻿// TrainingAlgorithm_CPP.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+
+int main()
+{
+    int appleAmount = 5;
+    int orangeAmount;
+
+    std::cin >> orangeAmount;
+    std::cout << appleAmount + orangeAmount << std::endl;
+    return 0;
+}
---
+++ ChapterOne/TrainingAlgorithm_CPP5/TrainingAlgorithm_CPP5/TrainingAlgorithm_CPP5.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+
+int N;
+std::string answer;
+
+int main() {
+	std::cin >> N;
+
+	//10進数を2進数に変換するプログラム
+	while (N >= 1) {
+		if (N % 2 == 0) { answer = "0" + answer; }
+		if (N % 2 == 1) { answer = "1" + answer; }
+
+		N = N / 2;
+	}
+
+	std::cout << answer << std::endl;
+	return 0;
+	
+}
